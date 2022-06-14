13efc9132354da895018d638954c0348b4fe8532
Daniil Gerasimyonok <gerasimenokd638@gmail.com>
2022-06-14 21:36:09 +0300

    feat: creat intro

--- game/js/intro.js
+++ game/js/intro.js
@@ -1,36 +1,40 @@
 let timedEvent;
-let bg;
-let shadow;
-let paul = {};
 let lexi = {};
+let paul = {};
+let textP;
+let textL;
 
 export function introPreload(sceneThis) {
     sceneThis.load.image("room", "assets/png/room.png");
     sceneThis.load.image("shadow", "assets/png/shadow.png");
     sceneThis.load.image("paul", "assets/png/paul.png");
-
-    sceneThis.load.image("lexi", "assets/png/lexi-0.png");
+    sceneThis.load.image("textP", "assets/png/paulText-0.png");
+    sceneThis.load.image("lexi-0", "assets/png/lexi-0.png");
+    sceneThis.load.image("textL", "assets/png/lexiText.png");
 };
 
 export function introCreate(sceneThis) {
-    bg = sceneThis.add.image(0, 0, "room");
+    let bg = sceneThis.add.image(0, 0, "room");
     bg.setOrigin(0, 0);
 
-    shadow = sceneThis.add.image(0, 0, "shadow");
+    let shadow = sceneThis.add.image(0, 0, "shadow");
     shadow.setOrigin(0, 0);
 
     paul = sceneThis.add.image(0, 0, "paul");
     paul.setOrigin(0, 0);
+    textP = sceneThis.add.image(300, 480, "textP");
+    textP.setScale(1);
 
-    lexi = sceneThis.add.image(-500, 0, "lexi");
+    lexi = sceneThis.add.image(-500, 0, "lexi-0");
     lexi.setOrigin(0, 0);
 };
 
 export function introUpdate(sceneThis) {
-    moveRight(sceneThis)
+    moveToTheRight(sceneThis);
+    text(sceneThis);
 };
 
-function moveRight(sceneThis) {
+function moveToTheRight(sceneThis) {
     timedEvent = sceneThis.time.delayedCall(800, function() {
         paul.x += 50;
         lexi.x += 50;
@@ -39,3 +43,11 @@ function moveRight(sceneThis) {
         if(lexi.x > 60) lexi.x = 60; 
     });
 };
+
+function text(sceneThis) {
+    if(paul.x > 0) textP.destroy();
+    if(lexi.x == 60) {
+        textL = sceneThis.add.image(300, 480, "textL");
+        textL.setScale(.9);
+    };
+}
